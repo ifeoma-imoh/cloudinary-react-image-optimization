ed09992e2efc97b08cf9cb6fbec40266d85674ef
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2021-10-04 14:14:00 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Cloudinary } from "@cloudinary/url-gen";
+import {
+  AdvancedImage,
+  lazyload,
+  responsive,
+  placeholder,
+} from "@cloudinary/react";
+// import { fill } from "@cloudinary/url-gen/actions/resize";
+import { Effect } from "@cloudinary/url-gen/actions/effect";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+import { byAngle } from "@cloudinary/url-gen/actions/rotate";
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: "ifeomaimoh",
+  },
+});
+const App = () => {
+  const file =
+    "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+  let image = cld.image(file);
+  image = image.setDeliveryType("fetch");
+  image = image
+    .effect(Effect.sepia())
+    // .resize(fill().height(500).width(490))
+    .roundCorners(byRadius().radius(40))
+    .rotate(byAngle(20))
+    .quality("q_40");
 
-function App() {
+  // This gives the full delivery URL of the tranformations applied above.
+  console.log({ url: image.toURL() });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "5rem" }}>
+      <h1 style={{ marginBottom: "50rem" }}>scroll to lazy load </h1>
+      <AdvancedImage
+        cldImg={image}
+        plugins={[lazyload("0", 1), responsive(200), placeholder("blur")]}
+      />
     </div>
   );
-}
-
+};
 export default App;
